f998e4214c496c6440607cd5576d953b841b40f2
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-11 17:56:48 +0100

    Add weather view with dynamic temperature data rendered in template

--- django_templates_project/template_app/views.py
+++ django_templates_project/template_app/views.py
@@ -5,3 +5,7 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request,"template_app/first.html")
+
+def weather(request):
+    weather_dictionary= {"Istanbul" : "30" , "Amsterdam" : "20" ,"Tel_Aviv" : "35"}
+    return render(request,"template_app/weather.html",context=weather_dictionary)
