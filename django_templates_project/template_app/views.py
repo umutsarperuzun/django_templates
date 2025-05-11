0c2ed1cb6e44c7505206c0f148d54c46996e979a
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-11 18:17:59 +0100

    Enhance weather view with nested data structures and update template rendering

--- django_templates_project/template_app/views.py
+++ django_templates_project/template_app/views.py
@@ -7,5 +7,11 @@ def index(request):
     return render(request,"template_app/first.html")
 
 def weather(request):
-    weather_dictionary= {"Istanbul" : "30" , "Amsterdam" : "20" ,"Tel_Aviv" : "35"}
+    weather_dictionary= {
+        "Istanbul" : "30" , 
+        "Amsterdam" : "20" ,
+        "Tel_Aviv" : "35" ,
+        "Paris" : [10,14,20,22] ,
+        "London" : {"Morning" : "7" ,"Noon" : "10" , "Afternoon": "12","Night" : "10"}
+        }
     return render(request,"template_app/weather.html",context=weather_dictionary)
