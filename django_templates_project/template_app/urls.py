d3588a80f7e345b2c8885f0ae26ffad1a7909781
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-12 13:27:56 +0100

    Add Django URL tag for linking to 'index' view of 'template_app'

--- django_templates_project/template_app/urls.py
+++ django_templates_project/template_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name="template_app"
+
 urlpatterns = [
     path("",views.index,name="index"),
     path("weather/",views.weather,name="weather")
